fd4a98a07bafb68859410967e079955c39679af4
Joseph-Marlow <jmarlow@byupathway.edu>
2025-08-02 09:40:29 -0700

    good enough?

--- scripts/form.js
+++ scripts/form.js
@@ -3,3 +3,18 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Dynamically display last modified date
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+const products = [
+      { name: "Smart Speaker X1" },
+      { name: "EcoFan Breeze 2000" },
+      { name: "SolarCharge 10K" },
+      { name: "AquaFilter Pro" }
+    ];
+
+    const productSelect = document.getElementById("product");
+    products.forEach(product => {
+      const option = document.createElement("option");
+      option.value = product.name;
+      option.textContent = product.name;
+      productSelect.appendChild(option);
+    });
